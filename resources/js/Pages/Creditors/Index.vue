efdaebc75ac190bad1e825bb76751a55566a367c
Vincy Deguma <vinsmar8888@gmail.com>
2024-03-18 15:44:36 +0800

    third commit

--- resources/js/Pages/Creditors/Index.vue
+++ resources/js/Pages/Creditors/Index.vue
@@ -4,11 +4,21 @@ import HeaderBar from '@/Components/HeaderBar.vue';
 import { ref } from 'vue';
 
 const props = defineProps({
-    creditor: Array,
+    creditor: Array, Object
 });
 
 const showDeleteModal = ref(false);
 const deleteCreditorId = ref(null);
+
+const form = useForm({
+    last_name: props.creditor.last_name,
+    first_name: props.creditor.first_name,
+    gender: props.creditor.gender,
+    birth_date: props.creditor.birth_date,
+    phone: props.creditor.phone,
+    address: props.creditor.address,
+    credit_limit: props.creditor.credit_limit,
+})
 
 const toggleDeleteModal = (show, creditorId = null) => {
     showDeleteModal.value = show;
@@ -25,6 +35,41 @@ const delCred = () => {
     });
     delForm.submit('delete', `/creditors/${deleteCreditorId.value}`);
     toggleDeleteModal(false);
+};
+
+const isModalOpen = ref(false);
+
+const Eform = ref({
+  last_name: '',
+  first_name: '',
+  gender: 'Male',
+  birth_date: '',
+  phone: '',
+  address: '',
+  credit_limit: ''
+});
+
+const toggleModal = () => {
+  isModalOpen.value = !isModalOpen.value;
+};
+
+const submitForm = () => {
+  console.log('Form submitted:', Eform.value);
+  // Here you can add your form submission logic
+  // After submission, you might want to reset the form
+  resetForm();
+};
+
+const resetForm = () => {
+  Eform.value = {
+    last_name: '',
+    first_name: '',
+    gender: 'Male',
+    birth_date: '',
+    phone: '',
+    address: '',
+    credit_limit: ''
+  };
 };
 
 </script>
@@ -64,10 +109,18 @@ const delCred = () => {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
                     </Link>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                        </svg>
+                    </button>
                     <button @click="() => toggleDeleteModal(true, c.id)" class="py-2 hover:text-red-600 rounded-lg">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>
+                    </button>
+                    <button @click="toggleModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                        Toggle modal
                     </button>
                 </div>
             </div>
@@ -84,4 +137,62 @@ const delCred = () => {
     </div>
     </div>
 
+
+  <!-- Main modal -->
+  <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Create New Product
+          </h3>
+          <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <form @submit.prevent="submitForm">
+          <div class="mb-3">
+            <label for="last_name">Last Name</label>
+            <input type="text" id="last_name" class="w-full" v-model="form.last_name">
+          </div>
+          <div class="mb-3">
+            <label for="first_name">First Name</label>
+            <input type="text" id="first_name" class="w-full" v-model="form.first_name">
+          </div>
+          <div class="mb-3">
+            <label for="gender">Gender</label>
+            <select id="gender" class="w-full" v-model="form.gender">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="birth_date">Birth Date</label>
+            <input type="date" id="birth_date" class="w-full" v-model="form.birth_date">
+          </div>
+          <div class="mb-3">
+            <label for="phone">Phone Number</label>
+            <input type="text" id="phone" class="w-full" v-model="form.phone">
+          </div>
+          <div class="mb-3">
+            <label for="address">Address</label>
+            <input type="text" id="address" class="w-full" v-model="form.address">
+          </div>
+          <div class="mb-3">
+            <label for="credit_limit">Credit Limit</label>
+            <input type="number" id="credit_limit" class="w-full" v-model="form.credit_limit">
+          </div>
+          <button type="submit" class="text-xl p-2.5 hover:text-green-600">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
 </template>
+
--- resources/js/Pages/Creditors/Show.vue
+++ resources/js/Pages/Creditors/Show.vue
@@ -103,12 +103,12 @@ const submit = () => {
                     <button type="submit" class="text-xl p-2.5 hover:text-green-600">Save</button>
                 </form>
                 </div>
-                <div class="flex justify-between m-5">
-                    <Link :href="'/creditors'" class="p-2.5 px-20 bg-green-800 text-white rounded hover:bg-green-600">
+                <Link :href="'/creditors'" class="p-2.5 px-20 bg-green-800 text-white rounded hover:bg-green-600">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                         </svg>
                     </Link>
+                <div class="flex justify-between m-5">
                     <button class="p-2.5 px-20 text-white bg-blue-800 rounded hover:bg-blue-600 "
                     @click="showEdit = !showEdit"> {{ showEdit ? 'Cancel' : 'Edit'}}
                     </button>
